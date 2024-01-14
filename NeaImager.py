c3437d441b20b1f1af1fcc9a3c89bb206cf6c743
Gergely Németh <ngergihun@gmail.com>
2024-01-14 21:03:36 +0100

    More correction methods added

--- NeaImager.py
+++ NeaImager.py
@@ -1,5 +1,7 @@
 import gwyfile
 import numpy as np
+import copy
+import math, cmath
 
 class NeaImage:
     def __init__(self) -> None:
@@ -19,7 +21,9 @@ class NeaImage:
         # Other parameters from info txt -  Dictionary
         self.parameters = None
 
-    def read_from_gwyfile(self):
+    def read_from_gwyfile(self,filename: str, channelname: str):
+        self.filename = filename
+        self.channel_name = channelname
         gwyobj = gwyfile.load(self.filename)
         channels = gwyfile.util.get_datafields(gwyobj)
         channel = channels[self.channel_name]
@@ -91,25 +95,95 @@ class NeaImage:
 
 ######### Correction functions ######
 # Line leveling
-def LineLevel(inputobj: NeaImage, type: str):
-    match type:
+def LineLevel(inputobj: NeaImage, mtype: str):
+    outputobj = copy.deepcopy(inputobj)
+    match mtype:
         case 'median':
             for i in range(inputobj.data.shape[0]):
                 if inputobj.isamp:
-                    inputobj.data[i][:] = inputobj.data[i][:]/np.median(inputobj.data[i][:])
+                    outputobj.data[i][:] = inputobj.data[i][:]/np.median(inputobj.data[i][:])
                 else:
-                    inputobj.data[i][:] = inputobj.data[i][:]-np.median(inputobj.data[i][:])
+                    outputobj.data[i][:] = inputobj.data[i][:]-np.median(inputobj.data[i][:])
         case 'average':
             for i in range(inputobj.data.shape[0]):
                 if inputobj.isamp:
-                    inputobj.data[i][:] = inputobj.data[i][:]/np.mean(inputobj.data[i][:])
+                    outputobj.data[i][:] = inputobj.data[i][:]/np.mean(inputobj.data[i][:])
                 else:
-                    inputobj.data[i][:] = inputobj.data[i][:]-np.mean(inputobj.data[i][:])
+                    outputobj.data[i][:] = inputobj.data[i][:]-np.mean(inputobj.data[i][:])
         case 'difference':
             for i in range(inputobj.data.shape[0]-1):
                 if inputobj.isamp:
                     c = np.median(inputobj.data[i+1][:]/inputobj.data[i][:])
-                    inputobj.data[i][:] = inputobj.data[i][:]/c
+                    outputobj.data[i][:] = inputobj.data[i][:]/c
                 else:
                     c = np.median(inputobj.data[i+1][:]-inputobj.data[i][:])
-                    inputobj.data[i][:] = inputobj.data[i][:]-c
+                    outputobj.data[i][:] = inputobj.data[i][:]-c
+    return outputobj
+
+def BackgroundPolyFit(inputobj: NeaImage, xorder: int, yorder: int):
+    outputobj = copy.deepcopy(inputobj)
+    Z = copy.deepcopy(outputobj.data)
+    x = list(range(0, outputobj.xres))
+    y = list(range(0, outputobj.yres))
+    X, Y = np.meshgrid(x,y)
+    x, y = X.ravel(), Y.ravel()
+
+    def get_basis(x, y, max_order_x=1, max_order_y=1):
+        """Return the fit basis polynomials: 1, x, x^2, ..., xy, x^2y, ... etc."""
+        basis = []
+        for i in range(max_order_y+1):
+            # for j in range(max_order_x - i +1):
+            for j in range(max_order_x+1):
+                basis.append(x**j * y**i)
+        return basis
+
+    basis = get_basis(x, y, xorder, yorder)
+    A = np.vstack(basis).T
+    b = Z.ravel()
+    c, r, rank, s = np.linalg.lstsq(A, b, rcond=None)
+
+    background = np.sum(c[:, None, None] * np.array(get_basis(X, Y, xorder, yorder)).reshape(len(basis), *X.shape), axis=0)
+
+    if inputobj.isamp:
+        outputobj.data = Z/background
+    else:
+        outputobj.data = Z-background
+
+    return outputobj, background
+
+def RotatePhase(inputobj: NeaImage, degree: float):
+    outputobj = copy.deepcopy(inputobj)
+
+    # Load amplitude image
+    if inputobj.isAmplitude():
+        pass
+    else:
+        ampIm = NeaImage()
+        channelname = inputobj.channel_name
+        new_channelname = channelname.replace('P','A')
+        ampIm.read_from_gwyfile(inputobj.filename,new_channelname)
+        ampIm.parameters = inputobj.parameters
+
+        # Complex map
+        C = cmath.rect(ampIm.data, inputobj.data + math.radians(degree))
+        outputobj.data = cmath.phase(C)
+
+    return outputobj
+
+def SelfReferencing(inputobj: NeaImage, order: int):
+    # Load amplitude image
+    outputobj = NeaImage()
+    if inputobj.isAmplitude():
+        channelname = f'O{order}A raw'
+    else:
+        channelname = f'O{order}P raw'
+
+    outputobj.read_from_gwyfile(inputobj.filename,channelname)
+    outputobj.parameters = inputobj.parameters
+
+    if inputobj.isAmplitude():
+        outputobj.data = np.divide(inputobj.data,outputobj.data)
+    else:
+        outputobj.data = inputobj.data-outputobj.data
+
+    return outputobj
--- test_neaimagemodule.py
+++ test_neaimagemodule.py
@@ -7,10 +7,7 @@ fname = r'C:\Users\ngerg\OneDrive\Python\NeaReader\2023-11-10 110444 PH.gwy'
 info_fname = 'testinfofile2.txt'
 channelname = 'O3P raw'
 meas = neaim.NeaImage()
-meas.channel_name = channelname
-meas.filename = fname
-meas.channel_name = channelname
-meas.read_from_gwyfile()
+meas.read_from_gwyfile(fname,channelname)
 meas.parameters = meas.read_info_file(info_fname)
 
 print(vars(meas))
