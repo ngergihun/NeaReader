645fd74244f8703c4c2d52606c42dbd8ec3186dc
Gergely Nemeth <ngergihun@gmail.com>
2024-01-15 14:44:51 +0100

    Small changes, naming

--- test_neaimagemodule.py
+++ test_neaimagemodule.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 import NeaImager as neaim
 
 # Load a Gwyddion file into memory
-fname = r'C:\Users\ngerg\OneDrive\Python\NeaReader\testPsHetImage.gwy'
-info_fname = 'testinfofile.txt'
+import os
+current_folder = os.getcwd()
+file_name = os.path.join(current_folder,'testPsHetImage.gwy')
+info_fname = os.path.join(current_folder,'testinfofile.txt')
 channelname = 'O3P raw'
 meas = neaim.NeaImage()
-meas.read_from_gwyfile(fname,channelname)
+meas.read_from_gwyfile(file_name,channelname)
 meas.parameters = meas.read_info_file(info_fname)
 
 print(vars(meas))
