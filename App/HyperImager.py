f3096159d6434ea8f84f36c493ed1754f594bfc1
Németh Gergely <nemeth.gergely@protonmail.com>
2024-02-24 21:58:44 +0100

    Fixes: image settings panel + loadinfo tree

--- App/HyperImager.py
+++ App/HyperImager.py
@@ -10,7 +10,8 @@ import NeaImager as neaim
 # from qt_material import apply_stylesheet
 
 current_folder = os.getcwd()
-ui_file = os.path.join(current_folder,'App\HyperImager.ui')
+ui_file = os.path.join(current_folder,'App\\HyperImager.ui')
+example_file = os.path.join(current_folder,'Examples\\testPsHetImage.gwy')
 
 uiclass, baseclass = pg.Qt.loadUiType(ui_file)
 
@@ -31,7 +32,7 @@ class MainWindow(uiclass, baseclass):
         self.prevButton.setText("\U0001F878")
         self.linelevelComboBox.addItems(['Mean','Median','Median of differences'])
         self.selfRefcomboBox.addItems(['1','2','3','4','5'])
-        self.colorbarcomboBox.addItems(['gray','fire','inferno','CET-D1','CET-D1A','CET-D3'])
+        self.colorbarcomboBox.addItems(['CET-L1','CET-L3','inferno','CET-D1','CET-D1A','CET-D3'])
         # setup stylesheet
         # apply_stylesheet(app, theme='light_blue.xml', invert_secondary=True)
 
@@ -48,17 +49,18 @@ class MainWindow(uiclass, baseclass):
         self.ApplyCorrectionButton.clicked.connect(self.ApplySingleCorrection)
         self.fileButton.toggled.connect(lambda: self.LeftMenuStack.setCurrentIndex(0))
         self.saveButton.toggled.connect(lambda: self.LeftMenuStack.setCurrentIndex(1))
+        self.colorbarcomboBox.currentTextChanged.connect(self.colormapChanged)
+        self.resetcolorbarButton.clicked.connect(lambda: self.update_image(self.measurement))
 
         # Create default plot
-        testdata = np.fromfunction(lambda i, j: (1+0.3*np.sin(i)) * (i)**2 + (j)**2, (100, 100))
-        testdata = testdata * (1 + 0.2 * np.random.random(testdata.shape) )
-        testdata = testdata.transpose()
-
+        self.current_file_name = example_file
+        self.measurement.filename = example_file
         # Create plot widget
         pg.setConfigOptions(imageAxisOrder='row-major')
-        self.imItem = pg.ImageItem(image=testdata)                                                  # create an ImageItem
+        self.imItem = pg.ImageItem()                                                  # create an ImageItem
         self.plotContainer.addItem(self.imItem)                                                         # add it to the PlotWidget
-        self.cbar = self.plotContainer.addColorBar(self.imItem,colorMap=self.colorMapName,rounding=0.01)        # Create a colorBarItem and add to the PlotWidget
+        self.cbar = self.plotContainer.addColorBar(self.imItem,colorMap=self.colorMapName,rounding=0.01)# Create a colorBarItem and add to the PlotWidget
+        self.cbar.sigLevelsChanged.connect(self.updateCbarSpinboxes)
         self.plotContainer.setBackground('w')
         axPen = pg.mkPen(color=(23, 54, 93), style=Qt.SolidLine, width = 1)
         axFont = QFont()
@@ -77,6 +79,7 @@ class MainWindow(uiclass, baseclass):
         self.plotContainer.getAxis('left').setLabel('Y position / μm')
         self.plotContainer.showAxes(True)
         self.plotContainer.setAspectLocked(True)
+        self.load_meas()
 
     def choose_file(self):
             fname = QFileDialog.getOpenFileName(self, "Choose GWY file","","Gwyddion files (*.gwy)")
@@ -111,7 +114,25 @@ class MainWindow(uiclass, baseclass):
     def update_image(self,m):
         self.imItem.setImage(image = m.data)
         self.cbar.setColorMap(pg.colormap.get(self.colorMapName))
-        self.cbar.setLevels(values = (np.min(m.data),np.max(m.data)))
+        if not self.cBcheckBox.isChecked():
+            print("not checked")
+            self.cbar.setLevels(values = (self.minCb.value(),self.maxCb.value()))
+        else:
+            self.cbar.setLevels(values = (np.min(m.data),np.max(m.data)))
+            self.minCb.setValue(np.min(m.data))
+            self.maxCb.setValue(np.max(m.data))
+        tr = QTransform()                                   # prepare ImageItem transformation:
+        tr.scale(m.xreal*1000, m.yreal*1000)                # scale horizontal and vertical axes
+        self.imItem.setTransform(tr)
+
+    def updateCbarSpinboxes(self):
+        v = self.cbar.levels()
+        self.minCb.setValue(v[0])
+        self.maxCb.setValue(v[1])
+
+    def colormapChanged(self,value):
+        self.colorMapName = value
+        self.update_image(self.measurement)
 
     def ApplySingleCorrection(self):
          methodidx = self.correctionsToolBox.currentIndex()
@@ -136,29 +157,34 @@ class MainWindow(uiclass, baseclass):
                   mtype = 'difference'
         m_levelled = neaim.LineLevel(inputobj = self.measurement, mtype = mtype)
         self.correctedMeasList.append(m_levelled)
-        self.update_image(self.correctedMeasList[-1])
+        self.measurement = self.correctedMeasList[-1]
+        self.update_image(self.measurement)
 
     def fitTheBackground(self):
         xdegree = self.bgDegreeXspinBox.value()
         ydegree = self.bgDegreeYspinBox.value()
         m_bg,f_bg = neaim.BackgroundPolyFit(inputobj = self.correctedMeasList[-1], xorder=xdegree, yorder=ydegree)
         self.correctedMeasList.append(m_bg)
-        self.update_image(self.correctedMeasList[-1])
+        self.measurement = self.correctedMeasList[-1]
+        self.update_image(self.measurement)
 
     def rotateThePhase(self):
         alpha = self.phaseRotSpinBox.value()
         m_rot = neaim.RotatePhase(inputobj = self.correctedMeasList[-1], degree = alpha)
         self.correctedMeasList.append(m_rot)
-        self.update_image(self.correctedMeasList[-1])
+        self.measurement = self.correctedMeasList[-1]
+        self.update_image(self.measurement)
 
     def selfReferencing(self):
         order = int(self.selfRefcomboBox.currentText())
         m_ref = neaim.SelfReferencing(inputobj = self.correctedMeasList[-1], order = order)
         self.correctedMeasList.append(m_ref)
-        self.update_image(self.correctedMeasList[-1])
+        self.measurement = self.correctedMeasList[-1]
+        self.update_image(self.measurement)
 
     # UI function staff
     def tree_from_dict(self, data=None, parent=None):
+        self.measinfo_treeWidget.clear()
         for key, value in data.items():
             item = QTreeWidgetItem(parent)
             item.setText(0, key)
